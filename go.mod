55376adeee2900b610cf5a18c114d59c27359210
line <line.xj.lin@gmail.com>
2021-12-28 17:25:30 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/linexjlin/zerolog
+
+go 1.15
+
+require github.com/rs/zerolog v1.26.1
---
+++ log.go
@@ -0,0 +1,43 @@
+package zerolog
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"os"
+	"time"
+)
+
+var debug = true
+var consoleWriter io.Writer
+var fileWriter io.Writer
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	consoleWriter = output
+	log.Logger = log.Output(output)
+	log.Logger = log.With().Caller().Logger()
+}
+
+func LogToFile(path string) {
+	if f, e := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660); e != nil {
+		log.Warn().Err(e)
+	} else {
+		fileWriter = f
+		multi := zerolog.MultiLevelWriter(consoleWriter, fileWriter)
+		log.Logger = log.Output(multi)
+		log.Logger = log.With().Caller().Logger()
+	}
+}
+
+func DebugEnable(enable bool) {
+	debug = enable
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+}
